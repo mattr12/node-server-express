9c0457d6fde6d6b39cc4c1f7fac4aaa5a459c738
Matt <mattrayes12@gmail.com>
2018-06-04 18:38:25 +0200

    Set up for Heroku, start script and port change

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ the .use() function is used to register middleware,
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -92,6 +93,6 @@ app.get('/test', (req,res) => {
 });
 
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log('Server is up on port', port);
 });
